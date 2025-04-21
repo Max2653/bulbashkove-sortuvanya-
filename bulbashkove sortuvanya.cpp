1fd108ee44b51662b59ff670acd9f971f3ea9c3f
eiviq <48763695+Max2653@users.noreply.github.com>
2025-04-21 16:15:30 +0300

    Add files via upload

---
+++ bulbashkove sortuvanya.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    
+    std::vector<int> arr(n);
+
+    
+    srand(static_cast<unsigned int>(time(0)));
+
+   
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 100 + 1;
+        std::cout << arr[i] << " "; 
+    }
+
+    std::cout << "\n";
+
+    
+    std::sort(arr.begin(), arr.end());
+
+    
+    for (int i = 0; i < n; i++) {
+        std::cout << arr[i] << " "; 
+    }
+
+    return 0;
+}
